c0a3a4ee2383bdb2f1aabbd8194e334d9f865e43
Nicola Vivante <s358312@studenti.polito.it>
2026-01-05 17:22:24 +0100

    Added missing check in product_route: barcode must be present when updating product

--- app/routes/product_route.py
+++ app/routes/product_route.py
@@ -210,6 +210,10 @@ async def update_product(product_id: int, product: ProductDTO):
 	if (product.quantity is not None and product.quantity < 0):
 		throw_bad_request()
 
+	# barcode must be present
+	if (product.barcode is None):
+		throw_bad_request()
+
 	# try to update product
 	product.id = product_id
 	await controller.update_product(product)
